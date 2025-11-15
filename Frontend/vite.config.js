1645fdd89584baa7536e0ed6ff808d5ef0767a1c
dhugoogalaanaa-stack <dhugoogalaanaa@gmail.com>
2025-11-15 12:24:13 +0300

    Fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5173}
-})
+  base: process.env.VITE_BASE_PATH || "/honey-shooping",
+  server: { port: 5173 },
+});
